5431469377d6776b928737c5be8df6f8d8097b52
IsmailSabryy <124688766+IsmailSabryy@users.noreply.github.com>
2024-08-23 17:23:13 +0300

    Update app.js

--- sample-application/app.js
+++ sample-application/app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Ismail !\n');
+  res.end('Hello, IBMers here is our demo for a dummy application!\n');
 });
 
 server.listen(port, hostname, () => {
